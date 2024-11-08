c29ffd51889d26d82ba203f9ee1d7eea9e62aa9f
Aditya <37655818+adi-075@users.noreply.github.com>
2024-11-08 11:36:46 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSET Library</title>
+</head>
+
+<body style="font-family: Arial, sans-serif;">
+    <div class="hero" style="text-align: center;">
+        <h1>Welcome to the Library App</h1>
+    </div>
+    <h2>Library App Home</h2>
+    <div class="app-home">
+        <p>Maintain <a href="">Students</a>.</p>
+        <p>Maintain <a href="books/books.php">books</a>.</p>
+        <p><a href="">Checkout</a> a book.</p>
+        <p><a href="">Return</a> a book.</p>
+        <p>Report a book <a href="">history</a>.</p>
+        <p>Application <a href="">logout</a>.</p>
+    </div>
+</body>
+
+</html>
