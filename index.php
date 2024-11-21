87f7d666daade0721ac775e9e3136f4aa0515bc3
Aditya <37655818+adi-075@users.noreply.github.com>
2024-11-21 17:16:16 -0500

    Refactor the Homepage

--- index.php
+++ index.php
@@ -16,20 +16,63 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CSET Library Homepage</title>
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap"
+        rel="stylesheet">
 </head>
+
 <body style="font-family: Arial, sans-serif;">
-<div class="hero" style="text-align: center;">
-    <h1>Welcome to the Library App</h1>
-</div>
-    <!-- <h2>Library App Home</h2> -->
+    <div class="hero" style="text-align: center;">
+        <h1>Library Homepage</h1>
+    </div>
+
     <div class="app-home">
-        <p>Maintain <a href="">Students</a>.</p>
-        <p>Maintain <a href="books/books.php">books</a>.</p>
-        <p><a href="">Checkout</a> a book.</p>
-        <p><a href="">Return</a> a book.</p>
-        <p>Report a book <a href="">history</a>.</p>
-        <p>Create <a href="create_user.php">Admin Account</a></p>
-        <p>Application <a href="logout.php">logout</a>.</p>
+        <div class="maintain">
+            <h3>
+                * Maintain
+            </h3>
+            <ul>
+                <li>
+                    <p>
+                        <a href="">Students</a>
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        <a href="books/books.php">Books</a>
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        <a href="create_user.php">Create Admin Account</a>
+                    </p>
+                </li>
+            </ul>
+        </div>
+        <br>
+        <div class="book-mgmt">
+            <h3>
+               * Book Management
+            </h3>
+            <ul>
+                <li>
+                    <p>
+                        <a href="books/checkout.php">Checkout Books</a>
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        <a href="books/return.php">Return Book</a>
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        <a href="books/book-history.php">View Book History</a>
+                    </p>
+                </li>
+            </ul>
+        </div>
     </div>
 </body>
 
